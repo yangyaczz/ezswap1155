04b030b1361d5a5665a180244e10171d4c5d7c8d
yyczz <a729746954@gmail.com>
2023-02-22 13:53:03 +0800

    clone

--- contracts/v2/lib/LSSVMPairCloner.sol
+++ contracts/v2/lib/LSSVMPairCloner.sol
@@ -200,7 +200,81 @@ library LSSVMPairCloner {
         ICurve bondingCurve,
         IERC1155 nft,
         uint8 poolType
-    ) internal returns (address instance) {}
+    ) internal returns (address instance) {
+        assembly {
+            let ptr := mload(0x40)
+
+            // -------------------------------------------------------------------------------------------------------------
+            // CREATION (9 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // creation size = 09
+            // runtime size = 72
+            // 60 runtime  | PUSH1 runtime (r)     | r                       | –
+            // 3d          | RETURNDATASIZE        | 0 r                     | –
+            // 81          | DUP2                  | r 0 r                   | –
+            // 60 creation | PUSH1 creation (c)    | c r 0 r                 | –
+            // 3d          | RETURNDATASIZE        | 0 c r 0 r               | –
+            // 39          | CODECOPY              | 0 r                     | [0-runSize): runtime code
+            // f3          | RETURN                |                         | [0-runSize): runtime code
+
+            // -------------------------------------------------------------------------------------------------------------
+            // RUNTIME (53 bytes of code + 61 bytes of extra data = 114 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // extra data size = 3d
+            // 3d          | RETURNDATASIZE        | 0                       | –
+            // 3d          | RETURNDATASIZE        | 0 0                     | –
+            // 3d          | RETURNDATASIZE        | 0 0 0                   | –
+            // 3d          | RETURNDATASIZE        | 0 0 0 0                 | –
+            // 36          | CALLDATASIZE          | cds 0 0 0 0             | –
+            // 3d          | RETURNDATASIZE        | 0 cds 0 0 0 0           | –
+            // 3d          | RETURNDATASIZE        | 0 0 cds 0 0 0 0         | –
+            // 37          | CALLDATACOPY          | 0 0 0 0                 | [0, cds) = calldata
+            // 60 extra    | PUSH1 extra           | extra 0 0 0 0           | [0, cds) = calldata
+            // 60 0x35     | PUSH1 0x35            | 0x35 extra 0 0 0 0      | [0, cds) = calldata // 0x35 (53) is runtime size - data
+            // 36          | CALLDATASIZE          | cds 0x35 extra 0 0 0 0  | [0, cds) = calldata
+            // 39          | CODECOPY              | 0 0 0 0                 | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 36          | CALLDATASIZE          | cds 0 0 0 0             | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 60 extra    | PUSH1 extra           | extra cds 0 0 0 0       | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 01          | ADD                   | cds+extra 0 0 0 0       | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | 0 cds 0 0 0 0           | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 73 addr     | PUSH20 0x123…         | addr 0 cds 0 0 0 0      | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            mstore(
+                ptr,
+                hex"60_72_3d_81_60_09_3d_39_f3_3d_3d_3d_3d_36_3d_3d_37_60_3d_60_35_36_39_36_60_3d_01_3d_73_00_00_00"
+            )
+            mstore(add(ptr, 0x1d), shl(0x60, implementation))
+
+            // 5a          | GAS                   | gas addr 0 cds 0 0 0 0  | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // f4          | DELEGATECALL          | success 0 0             | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | rds success 0 0         | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | rds rds success 0 0     | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 93          | SWAP4                 | 0 rds success 0 rds     | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 80          | DUP1                  | 0 0 rds success 0 rds   | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3e          | RETURNDATACOPY        | success 0 rds           | [0, rds) = return data (there might be some irrelevant leftovers in memory [rds, cds+0x37) when rds < cds+0x37)
+            // 60 0x33     | PUSH1 0x33            | 0x33 sucess 0 rds       | [0, rds) = return data
+            // 57          | JUMPI                 | 0 rds                   | [0, rds) = return data
+            // fd          | REVERT                | –                       | [0, rds) = return data
+            // 5b          | JUMPDEST              | 0 rds                   | [0, rds) = return data
+            // f3          | RETURN                | –                       | [0, rds) = return data
+            mstore(
+                add(ptr, 0x31),
+                hex"5a_f4_3d_3d_93_80_3e_60_33_57_fd_5b_f3_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00"
+            )
+
+            // -------------------------------------------------------------------------------------------------------------
+            // EXTRA DATA (61 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            mstore(add(ptr, 0x3e), shl(0x60, factory))
+            mstore(add(ptr, 0x52), shl(0x60, bondingCurve))
+            mstore(add(ptr, 0x66), shl(0x60, nft))
+            mstore8(add(ptr, 0x7a), poolType)
+
+            instance := create(0, ptr, 0x7b)
+        }
+    }
 
     function cloneERC20Pair1155(
         address implementation,
@@ -209,7 +283,82 @@ library LSSVMPairCloner {
         IERC1155 nft,
         uint8 poolType,
         ERC20 token
-    ) internal returns (address instance) {}
+    ) internal returns (address instance) {
+        assembly {
+            let ptr := mload(0x40)
+
+            // -------------------------------------------------------------------------------------------------------------
+            // CREATION (9 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // creation size = 09
+            // runtime size = 86
+            // 60 runtime  | PUSH1 runtime (r)     | r                       | –
+            // 3d          | RETURNDATASIZE        | 0 r                     | –
+            // 81          | DUP2                  | r 0 r                   | –
+            // 60 creation | PUSH1 creation (c)    | c r 0 r                 | –
+            // 3d          | RETURNDATASIZE        | 0 c r 0 r               | –
+            // 39          | CODECOPY              | 0 r                     | [0-runSize): runtime code
+            // f3          | RETURN                |                         | [0-runSize): runtime code
+
+            // -------------------------------------------------------------------------------------------------------------
+            // RUNTIME (53 bytes of code + 81 bytes of extra data = 134 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            // extra data size = 51
+            // 3d          | RETURNDATASIZE        | 0                       | –
+            // 3d          | RETURNDATASIZE        | 0 0                     | –
+            // 3d          | RETURNDATASIZE        | 0 0 0                   | –
+            // 3d          | RETURNDATASIZE        | 0 0 0 0                 | –
+            // 36          | CALLDATASIZE          | cds 0 0 0 0             | –
+            // 3d          | RETURNDATASIZE        | 0 cds 0 0 0 0           | –
+            // 3d          | RETURNDATASIZE        | 0 0 cds 0 0 0 0         | –
+            // 37          | CALLDATACOPY          | 0 0 0 0                 | [0, cds) = calldata
+            // 60 extra    | PUSH1 extra           | extra 0 0 0 0           | [0, cds) = calldata
+            // 60 0x35     | PUSH1 0x35            | 0x35 extra 0 0 0 0      | [0, cds) = calldata // 0x35 (53) is runtime size - data
+            // 36          | CALLDATASIZE          | cds 0x35 extra 0 0 0 0  | [0, cds) = calldata
+            // 39          | CODECOPY              | 0 0 0 0                 | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 36          | CALLDATASIZE          | cds 0 0 0 0             | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 60 extra    | PUSH1 extra           | extra cds 0 0 0 0       | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 01          | ADD                   | cds+extra 0 0 0 0       | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | 0 cds 0 0 0 0           | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 73 addr     | PUSH20 0x123…         | addr 0 cds 0 0 0 0      | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            mstore(
+                ptr,
+                hex"60_86_3d_81_60_09_3d_39_f3_3d_3d_3d_3d_36_3d_3d_37_60_51_60_35_36_39_36_60_51_01_3d_73_00_00_00"
+            )
+            mstore(add(ptr, 0x1d), shl(0x60, implementation))
+
+            // 5a          | GAS                   | gas addr 0 cds 0 0 0 0  | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // f4          | DELEGATECALL          | success 0 0             | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | rds success 0 0         | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3d          | RETURNDATASIZE        | rds rds success 0 0     | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 93          | SWAP4                 | 0 rds success 0 rds     | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 80          | DUP1                  | 0 0 rds success 0 rds   | [0, cds) = calldata, [cds, cds+0x35) = extraData
+            // 3e          | RETURNDATACOPY        | success 0 rds           | [0, rds) = return data (there might be some irrelevant leftovers in memory [rds, cds+0x37) when rds < cds+0x37)
+            // 60 0x33     | PUSH1 0x33            | 0x33 sucess 0 rds       | [0, rds) = return data
+            // 57          | JUMPI                 | 0 rds                   | [0, rds) = return data
+            // fd          | REVERT                | –                       | [0, rds) = return data
+            // 5b          | JUMPDEST              | 0 rds                   | [0, rds) = return data
+            // f3          | RETURN                | –                       | [0, rds) = return data
+            mstore(
+                add(ptr, 0x31),
+                hex"5a_f4_3d_3d_93_80_3e_60_33_57_fd_5b_f3_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00"
+            )
+
+            // -------------------------------------------------------------------------------------------------------------
+            // EXTRA DATA (81 bytes)
+            // -------------------------------------------------------------------------------------------------------------
+
+            mstore(add(ptr, 0x3e), shl(0x60, factory))
+            mstore(add(ptr, 0x52), shl(0x60, bondingCurve))
+            mstore(add(ptr, 0x66), shl(0x60, nft))
+            mstore8(add(ptr, 0x7a), poolType)
+            mstore(add(ptr, 0x7b), shl(0x60, token))
+
+            instance := create(0, ptr, 0x8f)
+        }
+    }
 
     /**
      * @notice Checks if a contract is a clone of a LSSVMPairETH.
